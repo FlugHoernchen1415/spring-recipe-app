88a261a18cea28f0aebce9c9a0f0c9332f517157
Rene <flughoernchen1415@gmail.com>
2018-06-12 22:52:43 +0200

    added read image from DB

--- src/main/java/reichhorn/spring/recipeapp/controllers/ImageController.java
+++ src/main/java/reichhorn/spring/recipeapp/controllers/ImageController.java
@@ -1,5 +1,6 @@
 package reichhorn.spring.recipeapp.controllers;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import reichhorn.spring.recipeapp.commands.RecipeCommand;
 import reichhorn.spring.recipeapp.services.ImageService;
 import reichhorn.spring.recipeapp.services.RecipeService;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @Controller
 public class ImageController {
@@ -34,5 +41,24 @@ public class ImageController {
         imageService.saveImageFile(Long.valueOf(recipeId), file);
 
         return "redirect:/recipe/" + recipeId + "/show";
+    }
+
+    @GetMapping("recipe/{recipeId}/recipeimage")
+    public void renderImageFromDB(@PathVariable String recipeId, HttpServletResponse response) throws IOException {
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+
+        if(recipeCommand.getImage() != null) {
+            byte[] byteArray = new byte[recipeCommand.getImage().length];
+
+            int i = 0;
+
+            for (Byte b : recipeCommand.getImage()) {
+                byteArray[i++] = b;
+            }
+
+            response.setContentType("image/jpeg");
+            InputStream is = new ByteArrayInputStream(byteArray);
+            IOUtils.copy(is, response.getOutputStream());
+        }
     }
 }
